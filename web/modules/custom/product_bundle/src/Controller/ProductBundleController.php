9101d0e4321ea514074d40a6e34fb511e01bcbe7
Eric Trinh <entrinh@ucsd.edu>
2019-08-19 09:30:43 -0700

    Added product bundle to add apps page

---
+++ web/modules/custom/product_bundle/src/Controller/ProductBundleController.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Drupal\product_bundle\Controller; 
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ProductBundleController extends ControllerBase {
+    public function content() {
+        return array(
+            '#type' => 'markup',
+            '#markup' => '<div> Working thing </div>',
+        );
+    }
+
+}
